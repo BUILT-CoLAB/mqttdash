1647bd5934b5c4ededeec7be4e36a58994ec801b
JP Dias <jpdias@live.com.pt>
2022-03-21 12:03:23 +0000

    added AC

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     M.AutoInit();
+
+    const inc = document.getElementsByClassName("stepper");
+    for (i = 0; i < inc.length; i++) {
+        var incI = inc[i].querySelector("input"),
+            id = incI.getAttribute("id"),
+            min = incI.getAttribute("min"),
+            max = incI.getAttribute("max"),
+            step = incI.getAttribute("step");
+        document
+            .getElementById(id)
+            .previousElementSibling.setAttribute(
+                "onclick",
+                "stepperInput('" + id + "', -" + step + ", " + min + ")"
+            );
+        document
+            .getElementById(id)
+            .nextElementSibling.setAttribute(
+                "onclick",
+                "stepperInput('" + id + "', " + step + ", " + max + ")"
+            );
+    }
+    function stepperInput(id, s, m) {
+        var el = document.getElementById(id);
+        if (s > 0) {
+            if (parseInt(el.value) < m) {
+                el.value = parseInt(el.value) + s;
+            }
+        } else {
+            if (parseInt(el.value) > m) {
+                el.value = parseInt(el.value) + s;
+            }
+        }
+    }
 });
+
+
 
 // Create a client instance
 client = new Paho.MQTT.Client("smarthub.iot.lan", Number(9001), "mqtt");
