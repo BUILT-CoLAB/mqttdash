ecbf92ee71098a09d72077044062557ea482eab8
JP Dias <jpdias@live.com.pt>
2022-10-21 14:16:03 +0100

    add eslint, prettier. refactor for allowing multi-line btns

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-restricted-syntax': 0,
+    'no-console': 0,
+    'no-undef': 0,
+    'no-restricted-globals': 0,
+  },
+};
